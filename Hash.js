af82fb645c93904714f4e722a59a40b5e262df77
Joseph Morales <josephzx1@hotmail.com>
2021-06-28 22:03:11 -0600

    Momento ultramamadisimo

--- Hash.js
+++ Hash.js
@@ -19,27 +19,24 @@ function setData(key, value) {
   if (!data[address]) {
     data[address] = [];
   }
-  const currentBucket = getData(key);
-  if (currentBucket) {
-    if (currentBucket < value) {
-      data[address].unshift([key, currentBucket]);
-    }
-    if (currentBucket > value) {
-      data[address].unshift([key, value]);
-    }
-  } else {
-    data[address].unshift([key, value]);
-  }
+  data[address].push([key, value]);
 }
 function getData(key) {
+  let menor = 0;
   const address = hash(key);
   const currentBucket = data[address];
   if (currentBucket) {
     for (let i = 0; i < currentBucket.length; i++) {
       if (currentBucket[i][0] == key) {
-        return currentBucket[i][1];
+        if (0 === i) {
+          menor = currentBucket[i][1];
+        }
+        if (menor > currentBucket[i][1]) {
+          menor = currentBucket[i][1];
+        }
       }
     }
+    return menor;
   }
   return false;
 }
@@ -48,6 +45,13 @@ for (let x = 0; x < matriz.length; x++) {
     setData(y, matriz[x][y]);
   }
 }
+let arr = [];
+for (let i = 0; i < matriz.length; i++) {
+  if (getData(i) || getData(i) === 0) {
+    arr.push(getData(i));
+  }
+}
+console.log("resultado final", arr);
 
 // setData(0, matriz[0][0]);
 // setData(0, matriz[1][0]);
@@ -55,6 +59,9 @@ for (let x = 0; x < matriz.length; x++) {
 // setData(1, matriz[0][1]);
 // setData(1, matriz[1][1]);
 // setData(1, matriz[2][1]);
-console.log(getData(0));
-console.log(getData(1));
-console.table(data);
+// console.log("menor de fila 0", getData(0));
+// console.log("menor de fila 1", getData(1));
+// console.log("menor de fila 2", getData(2));
+// console.log("menor de fila 3", getData(3));
+console.log(matriz.length);
+// console.table(data);
